d9639e7c7ceb5b76dcce642c667fa790a9f15c01
Akromjon <152626511+RustamovAkrom@users.noreply.github.com>
2024-10-06 11:21:05 +0500

    configure web exceptions

--- config/urls.py
+++ config/urls.py
@@ -1,19 +1,4 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf.urls import handler400, handler403, handler404, handler500
 from django.conf.urls.static import static
 from . import settings
 
@@ -22,8 +7,14 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls'))
+    path('', include('apps.blog.urls'))
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# handler400 = "apps.shared.views.bad_request_view"
+# handler403 = "apps.shared.views.page_permission_denied_view"
+# handler404 = "apps.shared.views.page_not_found_view"
+# handler500 = "apps.shared.views.server_error_view"
